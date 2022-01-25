2a355184511deb5e28e0500dbdc6e41c7e8617c7
GianlucaScarnicci <giangiscarnicci@gmail.com>
2022-01-25 17:18:04 +0100

    aggiunto get pagina home, get e post pagina crea, save e find nel service

--- src/main/java/org/generation/italy/controller/IndexController.java
+++ src/main/java/org/generation/italy/controller/IndexController.java
@@ -1,18 +1,46 @@
 package org.generation.italy.controller;
 
+
+import javax.validation.Valid;
+
+import org.generation.italy.model.Immobile;
+import org.generation.italy.service.ImmobiliService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 	
+	@Autowired
+	private ImmobiliService service;
+	
 	@GetMapping
 	public String index (Model model) {
-		
+		model.addAttribute("listaImmobili",service.trovaImmobile());
 		return "/immobili/index";
 	}
-
+	@GetMapping("/crea")
+	public String creaImmobile (Model model) {
+		model.addAttribute("edit", false);
+		model.addAttribute("immobile", new Immobile());
+		model.addAttribute("listaImmobili", service.trovaImmobile());
+		return "/amministrazione/formImmobile";
+	}
+	@PostMapping("/crea")
+	public String postImmobile(@Valid @ModelAttribute("immobile") Immobile formImmobile,BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("edit", false);
+			model.addAttribute("listaImmobili", service.trovaImmobile());
+			return "/amministrazione/formImmobile";
+		}
+		service.salvaImmobile(formImmobile);
+		return "redirect:/";
+	}
 }
--- src/main/java/org/generation/italy/service/ImmobiliService.java
+++ src/main/java/org/generation/italy/service/ImmobiliService.java
@@ -1,8 +1,22 @@
 package org.generation.italy.service;
 
+import java.util.List;
+
+import org.generation.italy.model.Immobile;
+import org.generation.italy.repository.ImmobiliRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImmobiliService {
-
+	
+	@Autowired
+	private ImmobiliRepository repository;
+	
+	public List<Immobile> trovaImmobile(){
+		return repository.findAll();
+	}
+	public Immobile salvaImmobile(Immobile immobile) {
+		return repository.save(immobile);
+	}
 }
